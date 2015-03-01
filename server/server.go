224b77b634e8256fc72ee0cbd6880c2f06fd4ae0
Joseph Miller <joff@googlehax.com>
2015-03-01 14:46:30 +1100

    Added very rubbish scaling

--- server/server.go
+++ server/server.go
@@ -7,12 +7,20 @@ import (
 
 func Start() {
     http.HandleFunc("/load", load)
+    http.HandleFunc("/thumb400", thumb400)
     http.ListenAndServe(":8080", nil)
 }
 
 func load(response http.ResponseWriter, request *http.Request) {
     response.Header().Set("Content-Type", "image/png")
 
-    artFile := twister.LoadImage("data/base.jpg")
-    twister.EncodePng(response, artFile)
+    image := twister.LoadImage("data/base.jpg")
+    twister.EncodePng(response, image)
+}
+
+func thumb400(response http.ResponseWriter, request *http.Request) {
+    response.Header().Set("Content-Type", "image/png")
+    image := twister.LoadImage("data/base.jpg")
+    scaled, _ := image.Scale(400, 400)
+    twister.EncodePng(response, scaled)
 }
