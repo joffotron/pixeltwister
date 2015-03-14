3d849e6b4c23c20d33c9c7319b5254a5234b9cd3
Joseph Miller <joff@googlehax.com>
2015-03-14 17:12:05 +1100

    Add unsharp mask function that I added to the magick lib!

--- image/twister.go
+++ image/twister.go
@@ -1,23 +1,28 @@
-// +build gm
+// x+build gm
 package image
 
 import (
-    m "gopkgs.com/magick.v1"
-    "io"
+	m "brutalbits.com/magick"
+	"io"
 )
 
 func LoadImage(filename string) *m.Image {
-    image, _ := m.DecodeFile(filename)
-    return image
+	image, _ := m.DecodeFile(filename)
+	return image
 }
 
 func EncodePng(output io.Writer, image *m.Image) {
-    info := m.NewInfo()
-    info.SetFormat("PNG")
-    image.Encode(output, info)
+	info := m.NewInfo()
+	info.SetFormat("PNG")
+	image.Encode(output, info)
 }
 
-func Scale(image *m.Image, x integer,y integer) *m.Image {
+func Scale(image *m.Image, x int, y int) *m.Image {
 	scaled, _ := image.Scale(x, y)
 	return scaled
+}
+
+func UnsharpMask(image *m.Image, radius float64, sigma float64, amount float64, threshold float64) *m.Image {
+	sharpened, _ := image.UnsharpMask(radius, sigma, amount, threshold)
+	return sharpened
 }
--- server/server.go
+++ server/server.go
@@ -1,25 +1,34 @@
 package server
 
 import (
-    twister "brutalbits.com/pixeltwister/image"
-    "net/http"
+	twister "brutalbits.com/pixeltwister/image"
+	"net/http"
 )
 
 func Start() {
-    http.HandleFunc("/load", load)
-    http.HandleFunc("/thumb400", thumb400)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/load", load)
+	http.HandleFunc("/thumb400", thumb400)
+	http.HandleFunc("/sharpThumb400", sharpThumb400)
+	http.ListenAndServe(":8080", nil)
 }
 
 func load(response http.ResponseWriter, request *http.Request) {
-    response.Header().Set("Content-Type", "image/png")
-
-    image := twister.LoadImage("data/base.jpg")
-    twister.EncodePng(response, image)
+	response.Header().Set("Content-Type", "image/png")
+	result := twister.LoadImage("data/base.jpg")
+	twister.EncodePng(response, result)
 }
 
 func thumb400(response http.ResponseWriter, request *http.Request) {
-    response.Header().Set("Content-Type", "image/png")
-    image := twister.LoadImage("data/base.jpg").twister.Scale(400,400)
-    twister.EncodePng(response, scaled)
+	response.Header().Set("Content-Type", "image/png")
+	image := twister.LoadImage("data/base.jpg")
+	result := twister.Scale(image, 400, 400)
+	twister.EncodePng(response, result)
+}
+
+func sharpThumb400(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "image/png")
+	image := twister.LoadImage("data/base.jpg")
+	scaled := twister.Scale(image, 400, 400)
+	result := twister.UnsharpMask(scaled, 0.0, 1.0, 0.9, 0.1)
+	twister.EncodePng(response, result)
 }
